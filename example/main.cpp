51ad57e07a0c3875ae4dbc8a778d135a06ede7f0
andre <andre@MAIN.local>
2021-10-31 23:03:26 +0300

    Update CMakeLists.txt, CMakeLists.txt, and main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,19 +1,38 @@
+// This is a nutclient.dll library usage example. If you have your Linux UPS server (NUT Server) configured to accept
+// remote connections, you can use this program to read the state of an UPS attached to the Linux machine from a Windows machine.
+// Usage:
+// upsinfo <host> <port>
+// where host and port correspond to those of the UPS Server.
+// "192.168.1.8", 3493
 #include <iostream>
+#include <string>
 #include <memory>
 #include "../nutclient.h"
 using namespace nut;
 using namespace std;
 
-int main()
+int main(int argc, char * argv[])
 {
-    std::unique_ptr<Client> client(new TcpClient("192.168.1.8", 3493));
-    auto devices = client->getDeviceNames();
-    for(const auto & d : devices)
-        cout << d << endl;
-    if (!devices.empty()) {
-        auto cmds = client->getDeviceCommandNames(*devices.begin());
-        for(const auto & c : cmds)
-            cout << c << endl;
+    if (argc != 3) {
+        cout << "Usage: upsinfo <host> <port>" << endl;
+        exit(-1);
     }
-    return 0;
+    auto host = string(argv[1]);
+    auto port = stoi(argv[2]);
+    std::unique_ptr<Client> client(new TcpClient(host, port));
+    auto devices = client->getDeviceNames();
+    for(const auto & device : devices) {
+        cout << device << endl;
+        if (!devices.empty()) {
+            auto variables = client->getDeviceVariableNames(device);
+            for (const auto & variable: variables) {
+                auto value = client->getDeviceVariableValue(device, variable);
+                cout << '\t' << variable << ": ";
+                for (const auto &v :value)
+                    cout << v << '\t';
+                cout << endl;
+            }
+        }
+    }
+    exit(0);
 }
