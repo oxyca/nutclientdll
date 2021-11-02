270661910b9f1dd6d59af847fe34cf708b61ee68
OXyCA <oxygencaffeine@gmail.com>
2021-11-02 12:45:28 +0300

    Update protocol.h

--- qt/qtnutclient/protocol.h
+++ qt/qtnutclient/protocol.h
@@ -1,15 +1,19 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
+#include <QObject>
+#include <QString>
+#include <list>
 
-
+namespace Internal {
 class Protocol : public QObject
 {
     Q_OBJECT
 public:
     explicit Protocol(QObject *parent = nullptr);
-
+    void connectToServer(const QString & host, int port);
+    std::list<QString> listDevices();
 signals:
 
 };
-
+}
 #endif // PROTOCOL_H
